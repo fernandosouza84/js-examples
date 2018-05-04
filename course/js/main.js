cf80eaaff4cfeae808076550503f10790dd939d5
Fernando Souza <fernando.de.souza@gmail.com>
2018-05-04 01:24:35 -0300

    adding format string

--- course/js/main.js
+++ course/js/main.js
@@ -27,13 +27,26 @@ function setList(list){
 
   for(var key in list){
       table += '<tr><th scope="row">'+key+'</th><td>'+
-      list[key].desc+'</td><td>'+
+      formatDesc(list[key].desc)+'</td><td>'+
       list[key].amount+'</td><td>'+
-      list[key].value+'</td><td>Edit | Delete</td></tr>';
+      formatValue(list[key].value)+'</td><td>Edit | Delete</td></tr>';
   }
 
   table +='</tbody>';
   document.getElementById("listTable").innerHTML = table;
+}
+
+function formatDesc(desc){
+    var str = desc.toLowerCase();
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    return str;
+}
+
+function formatValue(value){
+    var str = parseFloat(value).toFixed(2) + "";
+    str = str.replace(".",",");
+    str = "$ " + str;
+    return str;
 }
 
 setList(list);
