a3d81edfd7d82378460cf6611db4f208577dd2c5
Fernando Souza <fernando.de.souza@gmail.com>
2018-05-04 01:16:35 -0300

    adding table

--- course/js/main.js
+++ course/js/main.js
@@ -13,4 +13,28 @@ function getTotal(list){
     return total;
 }
 
+function setList(list){
+    var table = '<thead>'+
+    '<tr>'+
+      '<th scope="col">#</th>'+
+      '<th scope="col">Descrição</th>'+
+      '<th scope="col">Quantidade</th>'+
+      '<th scope="col">Valor</th>'+
+      '<th scope="col">Ação</th>'+
+    '</tr>'+
+  '</thead>'+
+  '<tbody>';
+
+  for(var key in list){
+      table += '<tr><th scope="row">'+key+'</th><td>'+
+      list[key].desc+'</td><td>'+
+      list[key].amount+'</td><td>'+
+      list[key].value+'</td><td>Edit | Delete</td></tr>';
+  }
+
+  table +='</tbody>';
+  document.getElementById("listTable").innerHTML = table;
+}
+
+setList(list);
 console.log(getTotal(list));
