da4ce3d515260563e4bfa689ddd8f7723b27309c
Fernando Souza <fernando.de.souza@gmail.com>
2018-05-16 23:43:20 -0300

    adding edit and delete js

--- course/js/main.js
+++ course/js/main.js
@@ -29,7 +29,8 @@ function setList(list){
       table += '<tr><th scope="row">'+key+'</th><td>'+
       formatDesc(list[key].desc)+'</td><td>'+
       list[key].amount+'</td><td>'+
-      formatValue(list[key].value)+'</td><td>Edit | Delete</td></tr>';
+      formatValue(list[key].value)+
+      '</td> <td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit</button> | <button class="btn btn-default" onclick="deleteData('+key+')">Delete</button></td></tr>';
   }
 
   table +='</tbody>';
@@ -58,6 +59,51 @@ function addData(){
 
     list.unshift({"desc":desc , "amount":amount ,"value":value });
     setList(list);
+}
+
+function setUpdate(id){
+    var obj = list[id];
+    document.getElementById('desc').value = obj.desc;
+    document.getElementById('amount').value = obj.amount;
+    document.getElementById('value').value = obj.value;
+    document.getElementById('bntUpdate').style.display = "inline-block";
+    document.getElementById('btnAdd').style.display = "none";
+    document.getElementById('inputIDUpdate').innerHTML = "<input type='hidden' id='idUpdate' value='"+id+"'>";
+}
+
+function resetForm(){
+    document.getElementById('desc').value = "";
+    document.getElementById('amount').value = "";
+    document.getElementById('value').value = "";
+    document.getElementById('bntUpdate').style.display = "none";
+    document.getElementById('btnAdd').style.display = "inline-block";
+    document.getElementById('inputIDUpdate').innerHTML = "";
+}
+
+function updateData(){
+    var id = document.getElementById("idUpdate").value;
+    var desc = document.getElementById('desc').value;
+    var amount = document.getElementById('amount').value;
+    var value = document.getElementById('value').value;
+
+    list[id] = {"desc":desc,"amount":amount, "value":value};
+    resetForm();
+    setList(list);
+}
+
+function deleteData(id){
+    if(confirm("Delete this item ?")){
+        if(id === list.length - 1){
+            list.pop();
+        } else if (id === 0) {
+            list.shift();
+        } else {
+            var arrAuxIni = list.slice(0,id);
+            var arrAuxEnd = list.slice(id + 1);
+            list = arrAuxIni.concat(arrAuxEnd);
+        }
+        setList(list);
+    }
 }
 
 setList(list);
